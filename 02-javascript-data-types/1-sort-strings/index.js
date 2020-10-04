5e5236444a6e2aa38d669eca96a585e2c5c336e5
Alex <ilovecoding.start@gmail.com>
2020-10-04 23:20:24 +0300

    the shortest implementation sort function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,10 +1,9 @@
+const defaultComparator = (a, b) => a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'});
+
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-  const defaultComparator = (a, b) => a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'});
-  return param === 'asc' ? [...arr].sort(defaultComparator) : [...arr].sort((a, b) => defaultComparator(b, a));
-}
+export const sortStrings = ([...arr], param = 'asc') => param === 'asc' ? arr.sort(defaultComparator) : arr.sort((a, b) => defaultComparator(b, a));
