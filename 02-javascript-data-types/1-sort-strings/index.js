ec89c41f5b2f61f3378cee781364a8deff10f29a
Alex <ilovecoding.start@gmail.com>
2020-10-04 16:46:42 +0300

    sort strings func implementation

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,3 +1,7 @@
+function copySorted(arr) {
+  return arr.slice().sort((a, b) => a.localeCompare(b, undefined, {caseFirst: 'upper'}));
+}
+
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
@@ -5,5 +9,5 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  return param === 'asc' ? copySorted(arr) : copySorted(arr).reverse();
 }
